d95d638e8b2f13ad99eeea5a58ee4678a17c9e7c
wangheyu <greatxsir@gmail.com>
2021-10-28 22:53:44 +0800

    add system call impl and load image impl

--- LC3VirtualMachine/first.cpp
+++ LC3VirtualMachine/first.cpp
@@ -54,6 +54,15 @@ enum {
     FL_NEG = 1 << 2, /* N */
 };
 
+enum {
+    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
+    TRAP_OUT = 0x21,   /* output a character */
+    TRAP_PUTS = 0x22,  /* output a word string */
+    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT = 0x25   /* halt the program */
+};
+
 uint16_t sign_extend(uint16_t x, int bit_count) {
     if ((x >> (bit_count - 1)) & 1) {
         x |= (0xFFFF << bit_count);
@@ -61,6 +70,7 @@ uint16_t sign_extend(uint16_t x, int bit_count) {
     return x;
 }
 
+char promotion = '|';
 
 void update_flags(uint16_t r) {
     if (reg[r] == 0) {
@@ -71,6 +81,39 @@ void update_flags(uint16_t r) {
     } else {
         reg[R_COND] = FL_POS;
     }
+}
+
+//LC3 program is big-endian, while most computers is little endian
+uint16_t swap16(uint16_t x) {
+    return (x << 8) | (x >> 8);
+}
+
+void read_image_file(FILE *file) {
+    /* the origin tells us where in memory to place the image */
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    /* we know the maximum file size so we only need one fread */
+    uint16_t max_read = UINT16_MAX - origin;
+    uint16_t *p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    /* swap to little endian */
+    while (read-- > 0) {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+
+
+int read_image(const char *image_path) {
+    FILE *file = fopen(image_path, "rb");
+    if (!file) { return 0; };
+    read_image_file(file);
+    fclose(file);
+    return 1;
 }
 
 int main(int argc, const char *argv[]) {
@@ -160,7 +203,14 @@ int main(int argc, const char *argv[]) {
             }
                 break;
             case OP_JSR: {
-
+                uint16_t long_flag = (instr >> 11) & 0x1;
+                reg[R_R7] = reg[R_PC];
+                if (long_flag) {
+                    reg[R_PC] += sign_extend(instr & 0x7FF, 12);
+                } else {
+                    uint16_t BaseR = (instr >> 6) & 0x7;
+                    reg[R_PC] = reg[BaseR];
+                }
             }
                 break;
             case OP_LD: {
@@ -183,11 +233,11 @@ int main(int argc, const char *argv[]) {
             }
                 break;
 
-            //load relative = load base+offset
+                //load relative = load base+offset
             case OP_LDR: {
                 uint16_t DR = (instr >> 9) & 0x3;
                 uint16_t BaseR = (instr >> 6) & 0x7;
-                uint16_t offset6 = sign_extend(instr & 0x1F, 6);
+                uint16_t offset6 = sign_extend(instr & 0x3F, 6);
 
                 reg[DR] = mem_read(reg[BaseR] + offset6);
                 update_flags(DR);
@@ -195,7 +245,7 @@ int main(int argc, const char *argv[]) {
             }
                 break;
 
-            //load effective address
+                //load effective address
             case OP_LEA: {
                 uint16_t DR = (instr >> 9) & 0x7;
                 uint16_t offset9 = sign_extend(instr & 0x1FF, 9);
@@ -203,30 +253,88 @@ int main(int argc, const char *argv[]) {
                 update_flags(DR);
             }
                 break;
-            //store:将寄存器里的值存到内存中
+                //store:将寄存器里的值存到内存中
             case OP_ST: {
                 uint16_t SR = (instr >> 9) & 0x7;
                 uint16_t address = reg[R_PC] + sign_extend(instr & 0x1FF, 9);
-                mem_set(address, reg[SR]);
+                mem_write(address, reg[SR]);
             }
                 break;
-            //store indirect
+                //store indirect
             case OP_STI: {
                 uint16_t SR = (instr >> 9) & 0x7;
                 uint16_t address = reg[R_PC] + sign_extend(instr & 0x1FF, 9);
-                mem_set(mem_read(address), reg[SR]);
+                mem_write(mem_read(address), reg[SR]);
             }
                 break;
-            //store relative base+
+                //store relative base+
             case OP_STR: {
                 uint16_t SR = (instr >> 9) & 0x7;
                 uint16_t BaseR = (instr >> 6) & 0x7;
                 uint16_t address = reg[BaseR] + sign_extend(instr & 0x3F, 6);
-                mem_set(address, reg[SR]);
+                mem_write(address, reg[SR]);
             }
                 break;
-            //system call
+                //system call
             case OP_TRAP: {
+                switch (instr & 0xFF) {
+                    case TRAP_GETC: {
+                        char c = getchar();
+                        reg[R_R0] = (uint16_t) c;
+                    }
+                        break;
+                    case TRAP_OUT: {
+                        char c = reg[R_R0];
+                        putc(c, stdout);
+                        fflush(stdout);
+                    }
+                        break;
+                    case TRAP_PUTS: {
+                        /* one char per word */
+                        uint16_t *c = memory + reg[R_R0];
+                        while (*c) {
+                            putc((char) *c, stdout);
+                            ++c;
+                        }
+                        fflush(stdout);
+                    }
+                        break;
+                    case TRAP_IN: {
+                        putc(promotion, stdout);
+                        char c = getchar();
+                        putc(c, stdout);
+                        reg[R_R0] = (uint16_t) c;
+                        fflush(stdout);
+                    }
+                        break;
+                    case TRAP_PUTSP: {
+                        uint16_t *c = memory + reg[R_R0];
+                        char c1 = (*c) & 0xFF, c2 = (*c >> 8);
+                        while (c1 && c2) {
+                            putc(c1, stdout);
+                            putc(c2, stdout);
+                            ++c;
+                            c1 = (*c) & 0xFF;
+                            c2 = (*c >> 8);
+                        }
+                        if (c1 == 0x00) {
+                            putc(0x00, stdout);
+                        } else {
+                            putc(c1, stdout);
+                            putc(0x00, stdout);
+                        }
+                        fflush(stdout);
+                    }
+                        break;
+                    case TRAP_HALT: {
+                        puts("HALT");
+                        fflush(stdout);
+                        running = 0;
+                    }
+                        break;
+                }
+
+
                 reg[R_R7] = reg[R_PC]++;
                 uint16_t trapvect8 = instr & 0xFF;
                 reg[R_PC] = mem_read(trapvect8);
